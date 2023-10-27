aec5274e13528bb246c0603e5f6a307610ae43ef
Gabriel Marquette <gabriel.marquette@reseau.eseo.fr>
2023-10-27 08:41:18 +0200

    Creation des différents fragments

--- Android/app/src/main/java/fr/pamarg/owndriveapp/MainActivity.java
+++ Android/app/src/main/java/fr/pamarg/owndriveapp/MainActivity.java
@@ -1,14 +1,38 @@
 package fr.pamarg.owndriveapp;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        navController = navHostFragment.getNavController();
     }
+
+    public void onClickFragProfile(View v)
+    {
+        navController.navigate(R.id.profileFragment);
+    }
+
+    public void onClickFragFiles(View v)
+    {
+        navController.navigate(R.id.filesFragment);
+    }
+
+    public void onClickFragTree(View v)
+    {
+        navController.navigate(R.id.treeFragment);
+    }
+
 }
---
+++ Android/app/src/main/java/fr/pamarg/owndriveapp/view/FilesFragment.java
@@ -0,0 +1,29 @@
+package fr.pamarg.owndriveapp.view;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import fr.pamarg.owndriveapp.R;
+
+public class FilesFragment extends Fragment
+{
+
+    public FilesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_files, container, false);
+    }
+}

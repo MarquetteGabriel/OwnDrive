13ec909a9866a4bfba55224b04ba6debc08fe04e
Gabriel Marquette <gabriel.marquette@reseau.eseo.fr>
2023-11-09 09:25:07 +0100

    Clean de merge failed

--- Android/app/src/main/java/fr/pamarg/owndriveapp/MainActivity.java
+++ Android/app/src/main/java/fr/pamarg/owndriveapp/MainActivity.java
@@ -1,10 +1,6 @@
 package fr.pamarg.owndriveapp;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,12 +14,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-import fr.pamarg.owndriveapp.Network.Connection;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
@@ -33,7 +23,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
 
+import fr.pamarg.owndriveapp.Network.Connection;
 import fr.pamarg.owndriveapp.view.treestructure.DrawerAdapter;
 import fr.pamarg.owndriveapp.view.treestructure.DrawerItem;
 import fr.pamarg.owndriveapp.view.treestructure.SubItems;
@@ -56,41 +48,6 @@ public class MainActivity extends AppCompatActivity implements DrawerAdapter.Dra
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.textView);
-
-        executorService = Executors.newSingleThreadExecutor();
-        mainActivityViewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
-        connection = new Connection(this, mainActivityViewModel);
-    }
-
-    public void onlickConnect(View v) {
-        try {
-            String ip = ((TextView) findViewById(R.id.editTextTextPersonName)).getText().toString();
-            Integer port = Integer.parseInt(((TextView) findViewById(R.id.editTextTextPersonName2)).getText().toString());
-            connection.connect(ip, port);
-        } catch (Exception e) {
-            Toast.makeText(this, "Erreur insert", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    public void checkState(View v)
-    {
-        if(Boolean.TRUE.equals(mainActivityViewModel.getState().getValue()))
-        {
-            Toast.makeText(this, "Connect", Toast.LENGTH_LONG).show();
-        }
-        else if(Boolean.FALSE.equals(mainActivityViewModel.getState().getValue()))
-        {
-            Toast.makeText(this, "No Connect", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    public void onClickgetIp(View v)
-    {
-        connection.getIpAddress();
-        textView.setText(mainActivityViewModel.getIpAddress().getValue());
-        ((EditText) findViewById(R.id.editTextTextPersonName)).setText(mainActivityViewModel.getIpAddress().getValue());
-    }
 
         final LinearLayout linearLayoutFiles = findViewById(R.id.linearLayoutFiles);
         final LinearLayout linearLayoutHome = findViewById(R.id.linearLayoutHome);

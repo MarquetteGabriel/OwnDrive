64a630a8e9bfd97d6abbd13d5ffb31e227d53fab
Gabriel Marquette <gabriel.marquette@reseau.eseo.fr>
2023-12-14 12:12:37 +0100

    Avancée du gridView via les card view Ajout de certaines images pour quelques extensions (à amplifier)

--- Android/app/src/main/java/fr/pamarg/owndriveapp/view/FilesFragment.java
+++ Android/app/src/main/java/fr/pamarg/owndriveapp/view/FilesFragment.java
@@ -4,70 +4,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import fr.pamarg.owndriveapp.R;
 import fr.pamarg.owndriveapp.model.treeManager.JsonManager;
+import fr.pamarg.owndriveapp.view.gridview.GridViewAdapter;
 import fr.pamarg.owndriveapp.viewmodel.MainActivityViewModel;
 
 public class FilesFragment extends Fragment
 {
 
-    String content = "{\n" +
-            "  \"file_manager\": {\n" +
-            "    \"root_directory\": \"/home/user/documents\",\n" +
-            "    \"current_directory\": \"/home/user/documents/work\",\n" +
-            "    \"files\": [\n" +
-            "      {\n" +
-            "        \"name\": \"file1.txt\",\n" +
-            "        \"type\": \"text\",\n" +
-            "        \"size\": \"1024 KB\",\n" +
-            "        \"created_at\": \"2023-01-01T12:00:00Z\",\n" +
-            "        \"modified_at\": \"2023-01-05T14:30:00Z\"\n" +
-            "      },\n" +
-            "      {\n" +
-            "        \"name\": \"file2.jpg\",\n" +
-            "        \"type\": \"image\",\n" +
-            "        \"size\": \"2048 KB\",\n" +
-            "        \"created_at\": \"2023-02-10T09:45:00Z\",\n" +
-            "        \"modified_at\": \"2023-02-12T11:20:00Z\"\n" +
-            "      },\n" +
-            "      {\n" +
-            "        \"name\": \"folder1\",\n" +
-            "        \"type\": \"directory\",\n" +
-            "        \"size\": \"N/A\",\n" +
-            "        \"created_at\": \"2023-03-20T15:10:00Z\",\n" +
-            "        \"modified_at\": \"2023-03-20T15:10:00Z\",\n" +
-            "        \"contents\": [\n" +
-            "          {\n" +
-            "            \"name\": \"subfile1.txt\",\n" +
-            "            \"type\": \"text\",\n" +
-            "            \"size\": \"512 KB\",\n" +
-            "            \"created_at\": \"2023-03-20T15:15:00Z\",\n" +
-            "            \"modified_at\": \"2023-03-20T15:45:00Z\"\n" +
-            "          },\n" +
-            "          {\n" +
-            "            \"name\": \"subfile2.txt\",\n" +
-            "            \"type\": \"text\",\n" +
-            "            \"size\": \"768 KB\",\n" +
-            "            \"created_at\": \"2023-03-20T15:20:00Z\",\n" +
-            "            \"modified_at\": \"2023-03-20T15:50:00Z\"\n" +
-            "          }\n" +
-            "        ]\n" +
-            "      }\n" +
-            "    ]\n" +
-            "  }\n" +
-            "}";
+    GridViewAdapter gridViewAdapter;
+    GridView gridView;
 
-    File jsonFile;
-    String fileName = "tree.json";
+    String[] textAnswer = {"Documents vraiment très personnel", "Images.jpg", "Videos.mp4", "Music.mp3", "Others.png", "Documents.docx", "Images.txt", "Videos.pdf", "Music", "Others.api", "Documents.java", "Images", "Videos.c", "Music", "Others"};
+
     MainActivityViewModel mainActivityViewModel;
     public FilesFragment() {
         // Required empty public constructor
@@ -83,16 +45,103 @@ public class FilesFragment extends Fragment
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_files, container, false);
         mainActivityViewModel = new ViewModelProvider(requireActivity()).get(MainActivityViewModel.class);
-        LinearLayout button = view.findViewById(R.id.add_folder);
-        button.setOnClickListener(view1 -> {
-            JsonManager.readDatas(requireActivity().getApplicationContext(), mainActivityViewModel);
-        });
         return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        gridView = view.findViewById(R.id.grid_view);
+
+        List<String> filesNameList = new ArrayList<>();
+
+        for (String fileName : textAnswer)
+        {
+            filesNameList.add(cropTextLength(fileName));
+        }
+        String[] filesName = new String[filesNameList.size()];
+        filesName = filesNameList.toArray(filesName);
+
+        int[] filesImages = setFilesImages(textAnswer);
+        gridViewAdapter = new GridViewAdapter(requireContext().getApplicationContext(), filesName, filesImages);
+        gridView.setAdapter(gridViewAdapter);
+
+        LinearLayout layoutAddFolder = view.findViewById(R.id.add_folder);
+        LinearLayout layoutAddFile = view.findViewById(R.id.add_file);
+        LinearLayout layoutAddPeople = view.findViewById(R.id.add_people);
+        LinearLayout layoutSearch = view.findViewById(R.id.search);
+        LinearLayout layoutMore = view.findViewById(R.id.more);
+
+        layoutAddFolder.setOnClickListener(view1 -> {
+            // TODO: add folder
+            JsonManager.getTreeFiles(mainActivityViewModel, mainActivityViewModel.getIpAddress().getValue());
+        });
+
+        layoutAddFile.setOnClickListener(view1 -> {
+            // TODO: add file
+        });
+
+        layoutAddPeople.setOnClickListener(view1 -> {
+            // TODO: add people
+        });
+
+        layoutSearch.setOnClickListener(view1 -> {
+            // TODO: search
+        });
+
+        layoutMore.setOnClickListener(view1 -> {
+            // TODO: more
+        });
+
+        gridView.setOnItemClickListener(this::gridViewonClick);
+    }
+
+    private void gridViewonClick(AdapterView<?> adapterView, View view12, int position, long l) {
+        Toast.makeText(requireContext().getApplicationContext(), textAnswer[position], Toast.LENGTH_SHORT).show();
+    }
+
+    private String cropTextLength(String text)
+    {
+        if (text.length() > 17)
+        {
+            return text.substring(0, 17) + "...";
+        }
+        return text;
+    }
+
+    private int[] setFilesImages(String[] filesType)
+    {
+        List<Integer> filesImagesList = new ArrayList<>();
+        for (String file : filesType) {
+            if (!file.contains(".")) {
+                filesImagesList.add(R.drawable.button_folder);
+            } else if (file.contains(".pdf")) {
+                filesImagesList.add(R.drawable.logo_pdf);
+            } else if (file.contains(".jpg") || file.contains(".png") || file.contains(".jpeg") ||
+                    file.contains(".gif")) {
+                filesImagesList.add(R.drawable.logo_image);
+            } else if (file.contains(".mp3")) {
+                filesImagesList.add(R.drawable.logo_mp3);
+            } else if (file.contains(".mp4")) {
+                filesImagesList.add(R.drawable.logo_mp4);
+            } else if (file.contains(".txt") || file.contains(".docx") || file.contains(".c") ||
+                    file.contains(".py") || file.contains(".java") || file.contains(".cpp") ||
+                    file.contains(".h") || file.contains(".html") || file.contains(".css") ||
+                    file.contains(".js") || file.contains(".php") || file.contains(".json") ||
+                    file.contains(".xml")) {
+                filesImagesList.add(R.drawable.logo_txt);
+            } else {
+                filesImagesList.add(R.drawable.logo_file);
+            }
+        }
+
+        int[] filesImages = new int[filesImagesList.size()];
+        for (int i = 0; i < filesImagesList.size(); i++)
+        {
+            filesImages[i] = filesImagesList.get(i);
+        }
+        return filesImages;
     }
 
 
---
+++ Android/app/src/main/java/fr/pamarg/owndriveapp/view/gridview/GridViewAdapter.java
@@ -0,0 +1,59 @@
+package fr.pamarg.owndriveapp.view.gridview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import fr.pamarg.owndriveapp.R;
+
+public class GridViewAdapter extends BaseAdapter {
+
+    private final Context context;
+    private final String[] fileName;
+    private final int[] fileIcon;
+
+    private LayoutInflater inflater;
+
+    public GridViewAdapter(Context context, String[] fileName, int[] fileIcon) {
+        this.context = context;
+        this.fileName = fileName;
+        this.fileIcon = fileIcon;
+    }
+
+    @Override
+    public int getCount() {
+        return fileName.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        if (inflater == null)
+            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        if (view == null){
+            view = inflater.inflate(R.layout.grid_item, null);
+        }
+
+        ImageView fileIcon_ImageView = view.findViewById(R.id.imageFiles);
+        TextView fileName_TextView = view.findViewById(R.id.textFiles);
+
+        fileIcon_ImageView.setImageResource(fileIcon[position]);
+        fileName_TextView.setText(fileName[position]);
+
+        return view;
+    }
+}
